3c301e9eba860aeed9fa4e51886d9d5a97764f93
James Liu <jamesliu1991@gmail.com>
2020-02-28 15:57:48 -0800

    written docstring and stub for describe_na_values

--- R/eda.R
+++ R/eda.R
@@ -15,3 +15,22 @@
 foo <- function(a) {
   print(a)
 }
+
+
+
+#' evaluates a dataframe for NA values.
+#'
+#' @param dataframe the dataframe to be inspected.
+#'
+#' @return a list of vectors; each vector corresponds to a column and
+#' each value inside the vector is 0 if the corresponding value is NA,
+#' 1 otherwise.
+#'
+#' @export
+#'
+#' @examples
+#' TODO write meaningful examples as implementation goes on
+describe_na_values <- function(dataframe) {
+  #TODO implement function
+  list(c(0))
+}
